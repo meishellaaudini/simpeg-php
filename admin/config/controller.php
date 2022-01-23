339de0bb2bbd70228825f540858b4fd20e622058
meishellaaudini <meishellaaudini@gmail.com>
2022-01-23 13:50:31 +0700

    buat login

--- admin/config/controller.php
+++ admin/config/controller.php
@@ -194,3 +194,65 @@ function hapus_pegawai($id_pegawai)
     return mysqli_affected_rows($db);
 }
 
+//----------------------------------------------------
+
+// fungsi tambah Akun
+function tambah_akun($post) 
+{
+    // panggil koneksi ke database
+    global $db;
+
+    // ambil input user
+    $nama = htmlspecialchars($post['nama']);
+    $no_telepon = htmlspecialchars($post['no_telepon']);
+    $role = htmlspecialchars($post['role']);
+
+    //enkripsi password ke database
+    $password = password_hash($password, PASSWORD_DEFAULT);
+
+    // query tambah data
+    $query = "INSERT INTO Akun VALUES(null, '$nama','$no_telepon', '$password', '$role')";
+
+    // simpan query ke database
+    mysqli_query($db, $query);
+
+    // check kolom yang bertambah
+    return mysqli_affected_rows($db);
+}
+
+// fungsi hapus Akun
+function hapus_akun($id_akun)
+{
+    global $db;
+
+    $query = "DELETE FROM akun WHERE id_akun = $id_akun";
+
+    mysqli_query($db, $query);
+
+    return mysqli_affected_rows($db);
+}
+
+// fungsi ubah Akun
+function ubah_akun($post) 
+{
+    // panggil koneksi ke database
+    global $db;
+
+    // ambil input user
+    $nama = htmlspecialchars($post['nama']);
+    $no_telepon = htmlspecialchars($post['no_telepon']);
+    $role = htmlspecialchars($post['role']);
+    $id_akun = $post['id_akun'];
+
+    //enkripsi password ke database
+    $password = password_hash($password, PASSWORD_DEFAULT);
+
+    // query ubah data
+    $query = "UPDATE Akun SET nama='$nama', no_telepon = '$no_telepon', role = '$role', password = '$password',WHERE id_akun = $id_akun";
+
+    // simpan query ke database
+    mysqli_query($db, $query);
+
+    // check kolom yang bertambah
+    return mysqli_affected_rows($db);
+}
--- admin/download-laporan.php
+++ admin/download-laporan.php
@@ -1,5 +1,7 @@
 <?php
 
+require 'config/core.php';
+
 require 'vendor/autoload.php';
 
 use PhpOffice\PhpSpreadsheet\Spreadsheet;
@@ -7,7 +9,55 @@ use PhpOffice\PhpSpreadsheet\Writer\Xlsx;
 
 $spreadsheet = new Spreadsheet();
 $sheet = $spreadsheet->getActiveSheet();
-$sheet->setCellValue('A1', 'Hello World !');
+$sheet->setCellValue('A4', 'No')->getColumnDimension('A')->setAutoSize(true);
+$sheet->setCellValue('B4', 'Nip')->getColumnDimension('B')->setAutoSize(true);
+$sheet->setCellValue('C4', 'Nama')->getColumnDimension('C')->setAutoSize(true);
+$sheet->setCellValue('D4', 'Bidang')->getColumnDimension('D')->setAutoSize(true);
+$sheet->setCellValue('E4', 'Jenis Kelamin')->getColumnDimension('E')->setAutoSize(true);
+$sheet->setCellValue('F4', 'Alamat')->getColumnDimension('F')->setAutoSize(true);
+$sheet->setCellValue('G4', 'Email')->getColumnDimension('G')->setAutoSize(true);
+$sheet->setCellValue('H4', 'Telepon')->getColumnDimension('H')->setAutoSize(true);
+$sheet->setCellValue('I4', 'Golongan')->getColumnDimension('I')->setAutoSize(true);
+$sheet->setCellValue('J4', 'gaji')->getColumnDimension('J')->setAutoSize(true);
+$sheet->setCellValue('K4', 'Status')->getColumnDimension('K')->setAutoSize(true);
+$sheet->setCellValue('L4', 'Terhitung Masa Kerja')->getColumnDimension('L')->setAutoSize(true);
+
+//query ke database
+$query = query("SELECT * FROM pegawai INNER JOIN bidang ON pegawai.id_bidang = bidang.id_bidang ORDER BY nama ASC");
+
+//Tampil data
+$no = 1;
+$m = 5;
+
+foreach ($query as $data) {
+    $sheet ->setCellValue('A'. $m, $no++);
+    $sheet ->setCellValue('B'. $m, $data['nip']);
+    $sheet ->setCellValue('C'. $m, $data['nama']);
+    $sheet ->setCellValue('D'. $m, $data['nama_bidang']);
+    $sheet ->setCellValue('E'. $m, $data['jk']);
+    $sheet ->setCellValue('F'. $m, $data['alamat']);
+    $sheet ->setCellValue('G'. $m, $data['email']);
+    $sheet ->setCellValue('H'. $m, $data['no_telepon']);
+    $sheet ->setCellValue('I'. $m, $data['golongan']); 
+    $sheet ->setCellValue('J'. $m, $data['gaji']);
+    $sheet ->setCellValue('K'. $m, $data['status']);
+    $sheet ->setCellValue('L'. $m, date('d/m/Y', strtotime($data['tmk'])));
+
+    $m++;
+}
+
+// styling
+$style = [
+    'borders' => [
+        'allBorders' => [
+            'borderStyle' => \PhpOffice\PhpSpreadsheet\Style\Border::BORDER_THIN,
+        ],
+    ],
+];
+
+$baris = $m - 1;
+$sheet->getStyle('A4:L' . $baris)->applyFromArray($style);
+
 
 $writer = new Xlsx($spreadsheet);
 $fileName = 'Contoh Laporan.xlsx';
